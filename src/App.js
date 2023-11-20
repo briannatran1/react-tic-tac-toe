19dfca652b6529a2878c55b2a07518c881677bcb
Brianna Tran <briannatran45@gmail.com>
2023-11-20 08:39:39 -0800

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-function Square() {
+import Square from "./Square";
+import { useState } from "react";
+
+/**
+ * Gameboard component
+ *
+ */
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
-      <button className="square">X</button>;
-      <button className="square">X</button>;
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+      </div>
     </>
   );
 }
 
-export default Square;
+export default Board;
